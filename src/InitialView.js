a37414d7466cca528c3ff51042cc8bc34c87c47e
Adstrat <73385073+Adstrat@users.noreply.github.com>
2022-02-08 21:55:57 +0100

    added initial pictures and caption on first render

---
+++ src/InitialView.js
@@ -0,0 +1,34 @@
+
+
+
+const InitialView = ( { initialPics } ) => {
+
+
+  return (
+    <div>
+      <div className="black-background container">
+        <h1 className="header">
+          <span className="explore">Explore .. </span>
+          <span className="discover">Discover .. </span>
+          <span className="inspire">Inspire</span>
+        </h1>
+        <div className="unsplash-pictures">
+          {initialPics.map( ( initialPic ) => (
+            <div
+              className="image"
+              key={initialPic.id}>
+              <img
+                className="card-image"
+                alt={initialPic.alt_description}
+                src={initialPic.urls.regular}
+                height="300px">
+              </img>
+            </div>
+          ) )}
+        </div>
+      </div>
+    </div >
+  );
+}
+
+export default InitialView;
--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import SearchResults from './SearchResults';
 import axios from 'axios';
+import InitialView from './InitialView';
 
 const Search = () => {
 
   const [query, setQuery] = useState( "" );
   const [pics, setPics] = useState( [] );
+  const [initialPics, setInitialPics] = useState( [] );
 
   const client_id = "tKjnMNUEn8rAuPmh5Zujwc26m6QVWXhsY0VaLV4HZ44";
   const fetchUrl = `https://api.unsplash.com/search/photos?client_id=${ client_id }&query=${ query }`;
@@ -29,20 +31,43 @@ const Search = () => {
       } )
   }
 
+  // initial photo selection 
+  useEffect( () => {
+    if ( pics.length < 1 ) {
+      axios
+        .get( `https://api.unsplash.com/photos?client_id=${ client_id }` )
+        .then( response => {
+          // this.setState({ imgs: data.data });
+          console.log( response );
+          setInitialPics( response.data );
+        } )
+        .catch( err => {
+          console.log( 'Error happened during fetching!', err );
+        } );
+    }
+  }, [] );
+
   return (
-    <div className="container">
-      <form
-        className="search-bar"
-        onSubmit={handleInput}
-      >
-        <input
-          type="text"
-          placeholder="Search for pictures..."
-          value={query}
-          onChange={handleChange}
+    <div>
+      <div className="container">
+        <form
+          className="search-bar"
+          onSubmit={handleInput}
+        >
+          <input
+            type="text"
+            placeholder="Search for pictures..."
+            value={query}
+            onChange={handleChange}
+          />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+      {pics.length < 1 &&
+        <InitialView
+          initialPics={initialPics}
         />
-        <button type="submit">Search</button>
-      </form>
+      }
       {pics.length > 0 &&
         <SearchResults
           pics={pics}
