5f12a50e5247af488907a20d2db5444d82f11bf6
Adstrat <73385073+Adstrat@users.noreply.github.com>
2022-02-08 21:56:56 +0100

    restyled
    black and white theme, larger fonts..

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -5,25 +5,28 @@ const SearchResults = ( { pics } ) => {
 
   return (
     <div>
-      <h1>Search Results:</h1>
-      <div className="unsplash-pictures">
-        {pics.map( ( pic ) => (
-          <div
-            className="image"
-            key={pic.id}>
-            <img
-              className="card-image"
-              alt={pic.alt_description}
-              src={pic.urls.regular}
-              height="200px">
-            </img>
-          </div>
-        ) )}
+      <div className="black-background container">
+        <h1 className="header">Search Results:</h1>
+        <div className="unsplash-pictures">
+          {pics.map( ( pic ) => (
+            <div
+              className="image"
+              key={pic.id}>
+              <img
+                className="card-image"
+                alt={pic.alt_description}
+                src={pic.urls.regular}
+                height="200px">
+              </img>
+            </div>
+          ) )}
+        </div>
       </div>
-    </div>
-
+    </div >
 
   );
 }
 
 export default SearchResults;
+
+
