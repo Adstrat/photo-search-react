f3f16adac1fdf687b83e2313036123a4e4169551
Adstrat <73385073+Adstrat@users.noreply.github.com>
2022-02-09 10:11:41 +0100

    Update Footer.js
    resolved typo className

--- src/Footer.js
+++ src/Footer.js
@@ -5,8 +5,8 @@
 const Footer = () => {
   return (
     <div className="black-background footer">
-      <h2 class="page-footer__text">&copy; Copyright 2022</h2>
-      <h2 class="page-footer__text">Designed and developed by
+      <h2 className="page-footer__text">&copy; Copyright 2022</h2>
+      <h2 className="page-footer__text">Designed and developed by
         <a href="https://adstrat.github.io/portfolio/" rel="noreferrer" target="_blank"> Andrew
           Stratton</a>
       </h2>
